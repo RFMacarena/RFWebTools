12b3e3c36380e71c28494d675b453cd9f6c6d1cd
RFMacarena <radiofisica.macarena@gmail.com>
2021-12-16 14:51:38 +0100

    Se integra el codigo de BallBearing de Jose y se implementa una ejecucion basica que funciona

--- rfwebtools/winstonlutz_app/urls.py
+++ rfwebtools/winstonlutz_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('winstonlutztest', views.winstonlutztest, name='winstonlutztest'),
+    path('bb_report/download_file/<fname>', views.download_file),
 ]
--- rfwebtools/winstonlutz_app/views.py
+++ rfwebtools/winstonlutz_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, Http404
 from .wl_form import WinstonLutzTest_Form
 from datetime import datetime
-#from ..BallBearing.Ball_Bearing_Winston_Lutz import Recta
-
+from BallBearing.Ball_Bearing_Winston_Lutz import Read_arguments, Get_Laser, Get_centro_radiacion
+from BallBearing.Ball_Bearing_Winston_Lutz import Evaluacion, Genera_Informe
+from django.conf import settings
+import os
 
 context = {}
 
@@ -16,14 +18,19 @@ def winstonlutztest(request):
             linac = form_winstonlutztest["linac_choice"].value()
             date1 = datetime.strptime(form_winstonlutztest['date1'].value(), "%Y-%m-%d")
             if linac:
+                run_winstonlutz()
                 message = f'Fecha: {date1}, ' \
                           f'ALE: {linac}, '
+                message_report = "Descargar informe de resultados"
+                context = {'form_winstonlutztest': form_winstonlutztest,
+                           'mensaje': message,
+                           'mensaje_informe': message_report}
 
             else:
                 message = "Debes rellenar algun campo."
+                context = {'form_winstonlutztest': form_winstonlutztest,
+                            'mensaje': message}
 
-            context = {'form_winstonlutztest': form_winstonlutztest,
-                       'mensaje': message}
         else:
             message = "Input invalido"
             context = {'form_winstonlutztest': form_winstonlutztest, 'mensaje': message}
@@ -33,4 +40,43 @@ def winstonlutztest(request):
         context = {'form_winstonlutztest': form_winstonlutztest}
         # context = {}
         return render(request, 'winstonlutz.html', context)
+
+def download_file(request,fname):
+    global df_resultados
+
+    file_path = os.path.join(settings.MEDIA_ROOT[0], fname)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="'application/pdf'")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
+
+def run_winstonlutz():
+
+    path_media = settings.MEDIA_ROOT[0]
+    fname = "Nombres_ficheros_winston_Lutz.txt"
+    argument_file = file_path = os.path.join(settings.MEDIA_ROOT[0], fname)
+    sigma = 2
+    low = 5
+    high = 30
+
+    # Lee nombres de los ficheros del fichero de argumentos self.argument_file y genera el array de "imagenes"
+    # Tambien las analiza y obtiene centro de radiacion y centro de los laseres.
+    ra = Read_arguments(argument_file, sigma, low, high)
+
+    # Calcula la posicion 3D del laser para gantry, colimador, mesa y todo combinado
+    print("Evaluando posicion laseres ...")
+    gl = Get_Laser(ra.imagenes)
+
+    # Calcula la posicion 3D del centro de radiacion para gantry, colimador, mesa y todo combinado
+    print("Evaluando posicion centro radiacion ...")
+    gc = Get_centro_radiacion(ra.imagenes)
+
+    # Evaluacion de resultados
+    print("Evaluando de resultados ...")
+    ge = Evaluacion(gl, gc)
+
+    # Generacion de Informe
+    Genera_Informe(path_media, ra.imagenes, gl, gc)
 
